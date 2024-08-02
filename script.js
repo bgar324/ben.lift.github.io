afd745a416dce760d423f96a0af4730dc8b2292f
bgar324 <161185715+bgar324@users.noreply.github.com>
2024-08-02 01:51:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var typed = new Typed(".auto-type", {
   strings: ["Computer Science Student","Bodybuilder","Videogamer"],
+  //speed for typing
   typeSpeed: 75,
+  //speed for backspacing
   backSpeed: 60,
   loop: true
 })
