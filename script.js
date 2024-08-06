2bf3be10d3c1dbf72d7e55abb425ccd1cda3f4a3
bgar324 <161185715+bgar324@users.noreply.github.com>
2024-08-05 20:03:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var img = new Image();
   img.src = 'images/IMG_1586.png';
   
-  var loadingBar = document.getElementById('loading-bar').querySelector('::after');
-  var progress = 0;
-
-  var interval = setInterval(function() {
-    progress += 10;
-    loadingBar.style.width = progress + '%';
-
-    if (progress >= 100) {
-      clearInterval(interval);
-    }
-  }, 300);
-
   img.onload = function () {
-    clearInterval(interval);
-    document.body.style.backgroundImage = "url('images/IMG_1586.png')";
     document.getElementById('loading-screen').style.display = 'none';
   };
 });
